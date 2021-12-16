3b34e24cf0ec9cc28d9ab4a72ba4446529f78916
Jaime Amate <jaime.amate@wefox.comjaime.amate@wefox.comjaime.amate@wefox.com>
2021-12-16 16:48:35 +0100

    Set up first level exercises

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello-world');
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`),
+);
