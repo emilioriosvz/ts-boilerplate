3af86dd6bc4464fa6af9dfdacb01b00268f131c2
Rosa <rosa.berned@wefox.com>
2021-12-23 12:44:01 +0100

    exercises done

--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -3,8 +3,12 @@ import app from '../../src/app';
 import * as usersRepo from '../../src/usersRepo';
 
 describe('/users', () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
     describe('GET /users', () => {
-        it('should retrive the list of users', async () => {
+        it('should retrieve the list of users', async () => {
             const getUsersMock = jest.spyOn(usersRepo, 'getUsers').mockReturnValue([
                 {
                     id: '1',
@@ -32,12 +36,15 @@ describe('/users', () => {
         });
 
         it('should return a user by username', async () => {
-            const getUserByUsernameMock = jest.spyOn(usersRepo, 'getUserByUsername').mockReturnValue({id: '__ID__', username: '__USERNAME__'})
+            const getUserByUsernameMock = jest.spyOn(usersRepo, 'getUserByUsername').mockReturnValue( {id: '__ID__', username: '__USERNAME__'} )
     
             const { status, body } = await request(app).get('/users?username=__USERNAME__');
     
-            expect({status, body}).toEqual({status: 200, body: {id: '__ID__', username: '__USERNAME__'}});
-            expect(getUserByUsernameMock.mock.calls).toEqual([['__USERNAME__']]);
+            expect({status, body}).toEqual({
+                status: 200, 
+                body: { id: '__ID__', username: '__USERNAME__' } 
+            });
+            expect(getUserByUsernameMock.mock.calls).toEqual( [ ['__USERNAME__'] ] );
         });
 
         it('should return a 404 status error if the username is not found', async () => {
@@ -45,19 +52,25 @@ describe('/users', () => {
             
             const { status, body } = await request(app).get('/users?username=__USERNAME__');
     
-            expect({ status, body}).toEqual({status: 404, body: { message: 'user not found'}});
-            expect(getUserByUsernameMock.mock.calls).toEqual([['__USERNAME__']]);
+            expect({ status, body}).toEqual({
+                status: 404, 
+                body: { message: 'user not found' }
+            });
+            expect(getUserByUsernameMock.mock.calls).toEqual( [ ['__USERNAME__'] ] );
         });
     });
 
     describe('GET /users/:id', () => {
         it('should return the user', async () => {
-            const getUserByIdMock = jest.spyOn(usersRepo, 'getUserById').mockReturnValue({id: '__ID__', username: '__USERNAME__'});
+            const getUserByIdMock = jest.spyOn(usersRepo, 'getUserById').mockReturnValue({ id: '__ID__', username: '__USERNAME__' });
 
             const { status, body } = await request(app).get('/users/__ID__');
 
-            expect({ status, body }).toEqual({status: 200, body: {id: '__ID__', username: '__USERNAME__'}})
-            expect(getUserByIdMock.mock.calls).toEqual([['__ID__']]);
+            expect({ status, body }).toEqual({
+                status: 200, 
+                body: { id: '__ID__', username: '__USERNAME__' }
+            });
+            expect(getUserByIdMock.mock.calls).toEqual( [ ['__ID__'] ] );
         });
         
         it('should return a 404 status code if the user is not found', async () => {
@@ -65,39 +78,51 @@ describe('/users', () => {
 
             const { status, body } = await request(app).get('/users/__ID__');
 
-            expect({ status, body}).toEqual({status: 404, body: { message: 'user not found'}});
-            expect(getUserByIdMock.mock.calls).toEqual([['__ID__']]);
+            expect({ status, body }).toEqual({
+                status: 404, 
+                body: { message: 'user not found' }
+            });
+            expect(getUserByIdMock.mock.calls).toEqual( [ ['__ID__'] ] );
         });
     });
 
     describe('POST /users', () => {
         it('should create a new user', async () => {
-            const createUserMock = jest.spyOn(usersRepo, 'createUser').mockReturnValue({id: '__ID__', username: '__USERNAME__'});
+            const createUserMock = jest.spyOn(usersRepo, 'createUser').mockReturnValue( {id: '__ID__', username: '__USERNAME__'} );
 
-            const { status, body } = await request(app).post('/users').send({username: '__USERNAME__'});
+            const { status, body } = await request(app).post('/users').send( {username: '__USERNAME__'} );
 
-            expect({status, body}).toEqual({status: 200, body: {id: '__ID__', username: '__USERNAME__'}});
-            expect(createUserMock.mock.calls).toEqual([[{username: '__USERNAME__'}]]);
+            expect( {status, body} ).toEqual({
+                status: 200, 
+                body: { id: '__ID__', username: '__USERNAME__' }
+            });
+            expect(createUserMock.mock.calls).toEqual([ [ {username: '__USERNAME__'} ] ]);
         });
     });
 
     describe('PUT /users/:id', () => {
         it('should update an existing user', async () => {
-            const updateUserMock = jest.spyOn(usersRepo, 'updateUser').mockReturnValue({id: '__ID__', username: '__USERNAME__'});
+            const updateUserMock = jest.spyOn(usersRepo, 'updateUser').mockReturnValue({ id: '__ID__', username: '__USERNAME__' });
 
-            const { status, body } = await request(app).put('/users/1').send({username: '__USERNAME__'});
+            const { status, body } = await request(app).put('/users/__ID__').send( { username: '__USERNAME__' } );
 
-            expect({status, body}).toEqual({status: 200, body: {id: '__ID__', username: '__USERNAME__'}})
-            expect(updateUserMock.mock.calls).toEqual([['__ID__', {username: '__USERNAME__'}]]);
+            expect({ status, body }).toEqual({
+                status: 200,
+                body: { id: '__ID__', username: '__USERNAME__' },
+            })
+            expect(updateUserMock.mock.calls).toEqual([['__ID__', { id: '__ID__', username: '__USERNAME__' }]]);
         });
 
         it('should return a 404 error if the user is not found', async () => {
             const updateUserMock = jest.spyOn(usersRepo, 'updateUser').mockReturnValue(undefined);
 
-            const { status, body } = await request(app).put('/users/__ID__').send({username: '__USERNAME__'});
+            const { status, body } = await request(app).put('/users/__ID__').send( {username: '__USERNAME__'} );
 
-            expect({ status, body}).toEqual({status: 404, body: { message: 'user not found'}});
-            expect(updateUserMock.mock.calls).toEqual([['__ID__', {username: '__USERNAME__'}]]);
+            expect({ status, body}).toEqual({
+                status: 404, 
+                body: { message: 'user not found' }
+            });
+            expect(updateUserMock.mock.calls).toEqual([ [ '__ID__', { id: '__ID__', username: '__USERNAME__' } ] ]);
         });
     });
 
@@ -105,19 +130,25 @@ describe('/users', () => {
         it('should delete an existing user', async () => {
             const deleteUserMock = jest.spyOn(usersRepo, 'deleteUser').mockReturnValue(true);
 
-            const { status, body } = await request(app).delete('/users/__ID__').send({username: '__USERNAME__'});
+            const { status, body } = await request(app).delete('/users/__ID__').send();
 
-            expect({status, body}).toEqual({ status: 204, body: {}});
-            expect(deleteUserMock.mock.calls).toEqual([['__ID__']]);
+            expect({ status, body }).toEqual({ 
+                status: 204, 
+                body: {}
+            });
+            expect(deleteUserMock.mock.calls).toEqual( [ ['__ID__'] ] );
         });
 
         it('should return a 404 error if the user is not found', async () => {
             const deleteUserMock = jest.spyOn(usersRepo, 'deleteUser').mockReturnValue(false);
 
-            const { status, body } = await request(app).delete('/users/__ID__').send({username: '__USERNAME__'});
+            const { status, body } = await request(app).delete('/users/__ID__').send();
 
-            expect({ status, body}).toEqual({status: 404, body: { message: 'user not found'}});
-            expect(deleteUserMock.mock.calls).toEqual([['__ID__']]);
+            expect({ status, body}).toEqual({
+                status: 404, 
+                body: { message: 'user not found' }
+            });
+            expect(deleteUserMock.mock.calls).toEqual( [ ['__ID__'] ] );
         });
     });
 });
